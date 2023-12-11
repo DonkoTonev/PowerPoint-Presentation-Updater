80f448c01a3ae58ed0b7e8d83eb80a5123577f47
DonkoTonev <donkotonev04@gmail.com>
2023-12-11 21:59:31 +0200

    .

--- print_cont.py
+++ print_cont.py
@@ -5,13 +5,21 @@ def extract_text_from_first_slide(pptx_file):
     presentation = Presentation(pptx_file)
 
     # Access the first slide
-    first_slide = presentation.slides[0]
+    second_slide = presentation.slides[1]
 
     # Extract text from shapes in the first slide
     text_content = []
-    for shape in first_slide.shapes:
-        if hasattr(shape, "text"):
-            text_content.append(shape.text)
+    for shape in second_slide.shapes:
+        # if hasattr(shape, "text"):
+        #     text_content.append(shape.text)
+        if shape.has_chart:
+            # text_content.append(shape)
+            chart = shape.chart
+            # You've found the chart; now you can update its data
+            break
+    new_data = [10, 20, 30, 40]  # replace with your actual new data
+    chart.replace_data(new_data)
+    presentation.save("C:\\Users\\admin\\OneDrive\\Desktop\\PowerPoint\\updated_presentation.pptx")
 
     return text_content
 
@@ -20,8 +28,8 @@ if __name__ == "__main__":
     pptx_file_path = "C:\\Users\\admin\\OneDrive\\Desktop\\Presentation_example4.pptx"
 
     # Extract text from the first slide
-    content = extract_text_from_first_slide(pptx_file_path)
+    extract_text_from_first_slide(pptx_file_path)
 
     # Print the extracted text content
-    for text in content:
-        print(text)
+    # for text in content:
+    #     print(text)
