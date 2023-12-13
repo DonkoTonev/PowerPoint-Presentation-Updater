916eee682ddc4716098cacfacce9b16b38106bd2
DonkoTonev <donkotonev04@gmail.com>
2023-12-13 09:35:05 +0200

    .

--- combined.py
+++ combined.py
@@ -3,11 +3,17 @@ from pptx.enum.chart import XL_CHART_TYPE
 from pptx.util import Inches, Pt
 from pptx.chart.data import CategoryChartData
 import csv
+from pptx.util import Inches
+from pptx.enum.shapes import MSO_SHAPE
+
 
 
 def update_presentation(presentation_path):
     # Open the presentation
     presentation = Presentation(presentation_path)
+    
+    slide_width = presentation.slide_width
+    slide_height = presentation.slide_height
 
     # Update the title of the first slide
     first_slide = presentation.slides[0]
@@ -17,8 +23,16 @@ def update_presentation(presentation_path):
     
     subtitle = first_slide.placeholders[1]  # Assuming the subtitle is the second placeholder
     subtitle.text = "New Subtitle"
+    
+    pic = "download.png"
+    
+   # left_inch = slide_width - width_inch  # 1 inch from the right edge
+    top_inch = 0   # 1 inch from the top
+    width_inch = Inches(4)  # adjust width as needed
+    height_inch = Inches(2) # adjust height as needed
+    left_inch = slide_width - width_inch  # 1 inch from the right edge
+    first_slide.shapes.add_picture(pic, left_inch, top_inch, width_inch, height_inch)
 
-    # Update the second slide with a new chart
     second_slide = presentation.slides[1]
     
     
@@ -63,45 +77,20 @@ def update_presentation(presentation_path):
             # Replace chart data with data from CSV
             chart.replace_data(chart_data)
             break
-
-    # Update the third slide with a new table cell
-    # third_slide = presentation.slides[2]
-    
-    # csv_file_path = 'data.csv'  # Update with your CSV file path
-    # with open(csv_file_path, 'r') as csv_file:
-    #     csv_reader = csv.reader(csv_file)
-    #     table_data = [row for row in csv_reader]
-
-
-    # # Iterate through shapes on the third slide
-    # for shape in third_slide.shapes:
-        
-        
-    #     # Check if the shape has a table
-    #     if shape.has_table:
-    #         # Iterate through rows and columns of the table
-    #         for i, row in enumerate(shape.table.rows):
-    #             # print(i, ':', row)
-    #             for j, cell in enumerate(row.cells):
-    #                 # print(j, ':', cell)
-                    
-    #                 # Replace the text in each cell with data from the CSV file
-    #                 if i < len(table_data) and j < len(table_data[i]):
-    #                     cell.text = table_data[i][j]
-    #                 else:
-    #                     cell.text = ''  # In case the CSV data is smaller than the table
-
-    #                 # Set the font size to 10 points
-    #                 cell.text_frame.paragraphs[0].runs[0].font.size = Pt(10)
-    
     
     third_slide = presentation.slides[2]
 
     # Update with your CSV file path
-    csv_file_path = 'data.csv'
+    csv_file_path = 'table.csv'
     with open(csv_file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         table_data = [row for row in csv_reader]
+        
+    
+    csv_bullets_file_path = 'bullets.csv'
+    with open(csv_bullets_file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        bullets_data = [row for row in csv_reader]
 
     # Iterate through shapes on the third slide
     for shape in third_slide.shapes:
@@ -118,22 +107,23 @@ def update_presentation(presentation_path):
 
                     # Set the font size to 10 points
                     cell.text_frame.paragraphs[0].runs[0].font.size = Pt(10)
-
-        # Check if the shape has bullets
+                    
         elif shape.has_text_frame and shape.text_frame.text != '':
             paragraphs = shape.text_frame.paragraphs
             # Iterate through paragraphs (assuming bullets are in separate paragraphs)
             for i, paragraph in enumerate(paragraphs):
                 # Replace the text in each paragraph with data from the CSV file
-                if i < len(table_data):
-                    paragraph.text = table_data[i][0]  # Assuming one column in the CSV for bullets
+                if i < len(bullets_data):
+                    paragraph.text = bullets_data[i][0]  # Assuming one column in the CSV for bullets
+
+                    # Keep the existing font size and styling for each run in the paragraph
+                    for run in paragraph.runs:
+                        # You may want to adjust this line based on your specific use case
+                        run.text = bullets_data[i][0]
                 else:
                     paragraph.text = ''  # In case the CSV data is smaller than the number of paragraphs
+                    # Clear the text in case there is no corresponding data in the CSV for this paragraph
 
-                # Set the font size to 10 points
-                for run in paragraph.runs:
-                    run.font.size = Pt(10)
-        
         
 
     # Save the updated presentation
